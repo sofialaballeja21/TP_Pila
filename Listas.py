f7ace3ee3e563b691e689f2f83fb8b69f0ced7ca
Sofia Laballeja <90810174+sofialaballeja21@users.noreply.github.com>
2022-09-22 09:08:53 -0300

    Create Listas.py

---
+++ Listas.py
@@ -0,0 +1,219 @@
+def criterio(dato, campo=None):
+    dic = {}
+    if(hasattr(dato, '__dict__')):
+        dic = dato.__dict__
+    if(campo is None or campo not in dic):
+        return dato
+    else:
+        return dic[campo]
+
+
+class nodoLista():
+    info, sig, sublista = None, None, None
+
+
+class Lista():
+
+    def __init__(self):
+        self.__inicio = None
+        self.__tamanio = 0
+
+
+    def insertar(self, dato, campo=None):
+        nodo = nodoLista()
+        nodo.info = dato
+        nodo.sublista = Lista()
+
+        if(self.__inicio is None or criterio(nodo.info, campo) < criterio(self.__inicio.info, campo)):
+            nodo.sig = self.__inicio
+            self.__inicio = nodo
+        else:
+            anterior = self.__inicio
+            actual = self.__inicio.sig
+            while(actual is not None and criterio(nodo.info, campo) > criterio(actual.info, campo)):
+                anterior = anterior.sig
+                actual = actual.sig
+
+            nodo.sig = actual
+            anterior.sig = nodo
+
+        self.__tamanio += 1
+
+    def lista_vacia(self):
+        return self.__inicio is None
+
+    def tamanio(self):
+        return self.__tamanio
+
+    def barrido(self):
+        aux = self.__inicio
+        while(aux is not None):
+            print(aux.info)
+            aux = aux.sig
+    
+    def barrido_entrenador_mas_tres(self):
+        aux = self.__inicio
+        while(aux is not None):
+            if(aux.info.torneos_ganados > 3):
+                print(aux.info)
+            aux = aux.sig
+    
+    def barrido_lista_lista(self):
+        aux = self.__inicio
+        while(aux is not None):
+            print(aux.info)
+            print('sublista:')
+            aux.sublista.barrido()
+            # aux1 = aux.sublista.__inicio
+            # while(aux1 is not None):
+            #     print('  ', aux1.info)
+            #     aux1 = aux1.sig
+
+            aux = aux.sig
+    
+    def barrido_armadura_traje(self):
+        aux = self.__inicio
+        while(aux is not None):
+            if('traje' in aux.info.bio or 'armadura' in aux.info.bio):
+                print(aux.info)
+            aux = aux.sig
+
+    def barrido_anterior_1963(self):
+        aux = self.__inicio 
+        while(aux is not None):
+            if(aux.info.aparicion < 1963):
+                print(aux.info)
+            aux = aux.sig
+                 
+    def barrido_anio(self, estreno):
+        aux = self.__inicio
+        while(aux is not None):
+            if(aux.info.AñoEstreno == estreno) :
+                print(aux.info)
+            aux = aux.sig
+    
+    def mayor(self, campo):
+        mayor = self.__inicio
+        aux = self.__inicio
+        while(aux is not None):
+            if(criterio(aux.info, campo) > criterio(mayor.info, campo)):
+                mayor = aux
+                break
+            aux = aux.sig
+        return mayor  
+    
+    def copiar(self, listaux, campo):
+        aux = self.__inicio
+        while(aux is not None):
+            listaux.insertar(aux.info, campo)
+            aux = aux.sig
+        
+    
+    def vaciar(self):
+        while( not self.lista_vacia()):
+            self.eliminar(self.obtener_elemento(0))
+            
+    def barrido_jedi_master(self):
+        aux = self.__inicio
+        while(aux is not None):
+            if('yoda' in aux.info.maestro or 'luke skywalker' in aux.info.maestro):
+                print(aux.info)
+            aux = aux.sig
+
+    def barrido_comienza_con(self, iniciales=[]):
+        aux = self.__inicio
+        while(aux is not None):
+            if(aux.info.nombre[0] in iniciales):
+                print(aux.info)
+            aux = aux.sig
+
+    def barrido_porcentaje_victorias(self):
+        aux = self.__inicio
+        while(aux is not None):
+            total = aux.info.batallas_ganadas + aux.info.batallas_perdidas
+            if(aux.info.batallas_ganadas / total >= 0.79):
+                print(aux.info)
+            aux = aux.sig
+            
+    def busqueda(self, buscado, campo=None):
+        pos = None
+        aux = self.__inicio
+        while(aux is not None and pos is None):
+            if(criterio(aux.info, campo) == buscado):
+                pos = aux
+            aux = aux.sig
+        return pos
+
+    def eliminar(self, clave, campo=None):
+        dato = None
+        if(criterio(self.__inicio.info, campo) == clave):
+            dato = self.__inicio.info
+            self.__inicio = self.__inicio.sig
+        else:
+            anterior = self.__inicio
+            actual = self.__inicio.sig
+            while(actual is not None and criterio(actual.info, campo) != clave):
+                anterior = anterior.sig
+                actual = actual.sig
+
+            if(actual is not None):
+                dato = actual.info
+                anterior.sig = actual.sig
+        if dato:
+            self.__tamanio -= 1 
+        if not self.lista_vacia():
+            if(criterio(self.__inicio.info, campo) == clave):
+                dato = self.__inicio.info
+                self.__inicio = self.__inicio.sig
+            else:
+                anterior = self.__inicio
+                actual = self.__inicio.sig
+                while(actual is not None and criterio(actual.info, campo) != clave):
+                    anterior = anterior.sig
+                    actual = actual.sig
+
+                if(actual is not None):
+                    dato = actual.info
+                    anterior.sig = actual.sig
+            if dato:
+                self.__tamanio -= 1 
+
+        return dato
+
+    def obtener_elemento(self, indice):
+        if(indice <= self.__tamanio-1 and indice >= 0):
+            aux = self.__inicio
+            for i in range(indice):
+                aux = aux.sig
+            return aux.info            
+        else:
+            return None
+
+    def contar_por_casa(self):
+        marvel, dc = 0, 0
+        aux = self.__inicio
+        while(aux is not None):
+            if(aux.info.casa.capitalize() == 'Marvel'):
+                marvel += 1
+            if(aux.info.casa.capitalize() == 'Dc'):
+                dc += 1
+            aux = aux.sig
+
+        return marvel, dc
+    
+    def mayor_de_lista(self, campo):
+        mayor = self.__inicio
+        aux = self.__inicio
+        while(aux is not None):
+            if(criterio(aux.info, campo) > criterio(mayor.info, campo)):
+                mayor = aux
+                break
+            aux = aux.sig
+        return mayor
+
+    def dino_level(self, lista, nivel):
+        aux = self.__inicio
+        while(aux is not None):
+            if(aux.info.nombre in lista and aux.info.level in nivel):
+                print(aux.info)
+            aux = aux.sig
