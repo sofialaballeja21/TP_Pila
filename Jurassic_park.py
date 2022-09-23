3d11acf4a4d9d0161e0a1c22601d8d61eaf84c8f
Sofia Laballeja <90810174+sofialaballeja21@users.noreply.github.com>
2022-09-23 18:29:57 -0300

    Add files via upload

--- Jurassic_park.py
+++ Jurassic_park.py
@@ -1,67 +1,115 @@
-
-from jurassic_park import dinosaurs
-from Listas import Lista
-from pila import Pila
+from lista import Lista
+from jurassic_parkkk import dinosaurs
 from cola import Cola
-
-class dino():
-    hora, zona, numero, estado, nombre = None, None, None,None, None
-    
-    def __str__(self):
-        return f'{park.hora}, {park.zona}, {park.numero}, {park.estado}, {park.nombre}'
 
 def busqueda(buscado):
     for dino in dinosaurs:
         if(int(buscado) == dino['number']):
-            return dino['name']
+            return dino['name'], dino['type']
 
-pila = Pila()
-lista_jurassic = Lista()
-lista = Lista()
-cola_carnivoros = Cola()
-cola_hervivoros = Cola()
+
+class Dinosaurio:
+    def __init__(self, fecha, codigo, nombre, level, type):
+        self.nombre = nombre
+        self.fecha = fecha
+        self.codigo = codigo
+        self.level = level
+        self.type = type
+
+    def __str__(self):
+        return f"nombre: {self.nombre}, fecha: {self.fecha}, zona: {self.codigo}, level: {self.level}, type: {self.type}"
+
+lista_fecha = Lista()
+lista_nombre = Lista()
+cola1 = Cola()
+cola2 = Cola()
 
 file = open('alerts.txt')
 
 lineas = file.readlines()
 lineas.pop(0)
- 
+
 for linea in lineas:
     dato = linea.split(';')
     dato[3] = dato[3][:-1]
-    dato.append(busqueda(dato[2]))
-    
-    park = dino()
-    park.hora   = dato[0]
-    park.zona   = dato[1]
-    park.numero = dato[2]
-    park.estado = dato[3]
-    park.nombre = dato[4]
-    
-    a = (f'{park.hora}, {park.zona}, {park.numero}, {park.estado}, {park.nombre}')
-    print(park.hora, park.zona, park.numero, park.estado, park.nombre)
-    pila.apilar(park)
-    print(park)
-#print(pila.tamanio())
+    name, type = busqueda(dato[2])
+    dino = Dinosaurio(dato[0], dato[1], name, dato[3], type)
+    lista_fecha.insertar(dino, 'fecha')
+    lista_nombre.insertar(dino, 'nombre')
+    if(dino.level in ['critical', 'high']):
+        if(dino.type == 'carnívoro '):
+            cola1.arribo(dino)
+        else:
+            cola2.arribo(dino)
 
+#lista_fecha.barrido()
+#print()
+#lista_nombre.barrido()
+
+park = ['Tyrannosaurus Rex', 'Spinosaurus', 'Giganotosaurus']
+lista_nombre.barrido_level(park, ['high', 'critical'])
+print()
+
+zonas = ['WYG075', 'SXH966', 'LYF010']
+for zona in zonas:
+    dato = lista_fecha.eliminar(zona, 'codigo')
+    if dato:
+        print(f'{dato} eliminada de la lista fecha')
+    dato = lista_nombre.eliminar(zona, 'codigo')
+    if dato:
+        print(f'{dato} eliminada de la lista nombre')
+
+
+pos = lista_fecha.busqueda('HYD195', 'codigo')
+if pos:
+    pos.info.nombre = 'nombre'
+
+pos = lista_nombre.busqueda('HYD195', 'codigo')
+if pos:
+    dato = lista_nombre.eliminar('HYD195', 'codigo')
+    dato.nombre = 'nombre'
+    lista_nombre.insertar(dato, 'nombre')
     
-while (not pila.pila_vacia()):
-   park = pila.desapilar()
- #Simon Masrani  
-   if (park.nombre == 'Tyrannosaurus Rex' and park.estado == 'critical' ):
-       print(f'Tirannosaurus Rex con estado critical:  {park.hora}, {park.zona}, {park.numero} ')
-       
-   if (park.nombre == 'Giganotosaurus' and park.estado == 'critical'):   
-         print(f'Giganotosaurus con estado critical:  {park.hora}, {park.zona}, {park.numero}')
-    
-   if (park.nombre == 'Spinosaurus' and park.estado == 'critical'):
-           print(f'Spinosaurus con estado critical:  {park.hora}, {park.zona}, {park.numero}') 
-             
-   if (park.nombre == 'Tyrannosaurus Rex' and park.estado == 'high'):
-       print(f'Tyrannossaurus Rex con estado high: {park.hora}, {park.zona}, {park.numero}')   
-  
-   if (park.nombre == 'Spinosaurus' and park.estado == 'high'):   
-         print(f'Sapitnosaurus con estado high:  {park.hora}, {park.zona}, {park.numero}')
- 
-   if (park.nombre == 'Giganotosaurus' and park.estado == 'high'):   
-         print(f'Giganotosaurus con estado high:  {park.hora}, {park.zona}, {park.numero}')    
+print('----------------------------------------------')
+
+lista_fecha.barrido()
+print('------------------------------------------------')
+lista_nombre.barrido()
+print('-------------------------------------------------')
+
+dinos = ['Compsognathus','Carnotaurus','Raptors (Dromaeosauridae)',]
+lista_nombre.barrido_nivel(dinos, ['high', 'critical', 'low', 'medium'])
+
+print()
+clave = 'mosquito'
+
+def decodificar(num):
+    if(num >= 33 and num <= 47):
+        return chr(numero)
+    else:
+        if(numero % 3 == 0):
+            return decodificar(num // 2 + 9)
+        else:
+            return decodificar(num-14)
+
+clave_deco = ''
+for palabra in clave:
+    numero = ord(palabra)
+    clave_deco += decodificar(numero)
+
+print(f'clave decodificada: {clave_deco}')
+
+print('---')
+while(not cola1.cola_vacia()):
+    dato = cola2.atencion()
+    if(dato.codigo == 'EPC944'):
+        print(' ha el dato ha sido borrado')
+    else:
+        print(dato)
+print('---')
+while(not cola1.cola_vacia()):
+    dato = cola2.atencion()
+    if(dato.codigo == 'EPC944'):
+        print(' ha el dato ha sido borrado')
+    else:
+        print(dato)
